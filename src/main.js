e361abd8257ed1ba1bdc87ef32c258fa6e7c6049
Deniz Yardımcı <99015184+dnzyrdmc@users.noreply.github.com>
2025-07-22 15:50:53 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+createApp(App).use(router).mount('#app').component('VueDatePicker', VueDatePicker).app.mount('#app');
